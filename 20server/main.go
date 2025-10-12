e2e3b8ae5ee405e47e071e60de65dd364bcec785
jaisriv788 <srivastavajai845@gmail.com>
2025-10-12 22:07:55 +0530

    completed upto lecture 29

---
+++ 20server/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+func main() {
+	fmt.Println("Welcome to server")
+
+	getRequest()
+	postRequest()
+}
+
+func getRequest() {
+	const myurl = "http://localhost:3000/get"
+
+	res, err := http.Get(myurl)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer res.Body.Close()
+
+	fmt.Println("Status code: ", res.StatusCode)
+	fmt.Println("Length: ", res.ContentLength)
+
+	var responseString strings.Builder
+	content, _ := io.ReadAll(res.Body)
+	bytecount, _ := responseString.Write(content)
+
+	fmt.Println("Bytecount is: ", bytecount)
+	fmt.Println(responseString.String())
+
+	fmt.Println(string(content))
+}
+
+func postRequest() {
+	const myurl = "http://localhost:3000/get"
+
+	requestBody := strings.NewReader(`
+	{
+	"name" : "Jai Srivastava",
+	"age":"23"
+	}
+	`)
+
+	response, err := http.Post(myurl, "application/json", requestBody)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer response.Body.Close()
+
+	var responseString strings.Builder
+	content, _ := io.ReadAll(response.Body)
+	bytecount, _ := responseString.Write(content)
+
+	fmt.Println("Bytecount is: ", bytecount)
+	fmt.Println(responseString.String())
+}
